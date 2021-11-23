0bbb71183e0ae59a3ae16ed274771f42fab3de0c
dahored <dahored@users.noreply.github.com>
2021-11-23 03:33:23 +0000

    deploy: cd1222437f41f7044f0af4e12ec2cb8ce3627b5a

---
+++ _nuxt/static/1637638402/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Faxiemanagertracker.github.io\u002F_nuxt\u002Fstatic\u002F1637638402",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Faxiemanagertracker.github.io\u002F",assetsPath:"\u002Faxiemanagertracker.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
